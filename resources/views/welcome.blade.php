afb347bb79516044ee6e960cc166d0d55ef39986
thinkverse <hallberg.kim@gmail.com>
2022-09-26 23:28:08 +0200

    Load `app.js` with `vite` directive

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,6 +19,8 @@
                 font-family: 'Nunito', sans-serif;
             }
         </style>
+
+        @vite('resources/js/app.js')
     </head>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
