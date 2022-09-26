271514ae963e7e6cfb4a6a013cd3de8e5adf10fe
thinkverse <hallberg.kim@gmail.com>
2022-09-26 23:24:26 +0200

    Import `jquery`

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,6 +1,9 @@
 import _ from 'lodash';
 window._ = _;
 
+import $ from 'jquery';
+window.$ = $;
+
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
